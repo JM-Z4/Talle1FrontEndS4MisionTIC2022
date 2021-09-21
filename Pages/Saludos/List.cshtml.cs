1a397110188d3f7c9b47b96ad96d0827126d727f
Jorge Motanez <analista1_desarrollo@clinicamedical.com.co>
2021-09-21 07:47:53 -0500

    SaludosCompletos

--- Pages/Saludos/List.cshtml.cs
+++ Pages/Saludos/List.cshtml.cs
@@ -4,18 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Pruebas1.App.Persistencia;
+using Pruebas1.App.Dominio;
 
 namespace Pruebas1.App.FrontEnd.Pages
 {
     public class ListModel : PageModel
     {
-        private string[] Saludos={"Buen día","Buena tarde","Buena noche","Hola","Bienvenido","Hasta mañana"};
-        public List<string> ListaSaludos{get;set;}
+        //private string[] Saludos={"Buen día","Buena tarde","Buena noche","Hola","Bienvenido","Hasta mañana"};
+        //public List<string> ListaSaludos{get;set;}
+        private readonly IRespositorioSaludos repsitoriosaludos;
+        public IEnumerable<Saludo> Saludos {get;set;}
+
+        public ListModel(IRespositorioSaludos repsitoriosaludos)
+        {
+            this.repsitoriosaludos=repsitoriosaludos;
+        }
 
         public void OnGet()
         {
-            ListaSaludos = new List<string>();
-            ListaSaludos.AddRange(Saludos);
+            //ListaSaludos = new List<string>();
+            //ListaSaludos.AddRange(Saludos);
+            Saludos=repsitoriosaludos.GetAll();
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Pruebas1.App.Persistencia;
 
 namespace Pruebas1.App.FrontEnd
 {
@@ -24,6 +25,7 @@ namespace Pruebas1.App.FrontEnd
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<IRespositorioSaludos, RepostorioSaludosMemoria>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
